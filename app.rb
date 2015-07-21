8a274420e2b33e09ace1dcdf24b13892a9da2da9
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-07-20 23:10:12 -0400

    Render kittens in json

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'bundler'
 Bundler.require
 
 require './config.rb'
+require './models/kitten.rb'
+require 'sinatra/json'
 
 get '/' do 
 	erb :index
@@ -9,20 +11,21 @@ end
 
 get '/random' do 
 	num_kittens = Kitten.all.size
-	Kitten.find(num_kittens - 1).to_json
+	json kitten: Kitten.find(num_kittens - 1).url
 end
 
 
 get '/bomb' do
-	count = params[:count]
+	count = params[:count].to_i
 	num_kittens = Kitten.all.size
 	kitten_ids = []
 	while kitten_ids.length < count do
-		rand_num = rand(0..num_kittens - 1)
+		rand_num = rand(1..num_kittens)
 		kitten_ids.push(rand_num) if !kitten_ids.include?(rand_num)
 	end
 
 	kittens = kitten_ids.map do |id|
 		Kitten.find(id)
 	end
+	json kittens: kittens.map { |kitten| kitten.url }
 end
